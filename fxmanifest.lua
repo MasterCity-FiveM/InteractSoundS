66b2a7600c0b75803ba308bb0502c54ac02c4f96
MasterkinG32 <lichwow_masterking@yahoo.com>
2020-12-05 16:35:11 -0800

    Added more sounds.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -17,5 +17,15 @@ files({
     'client/html/index.html',
 	'client/html/sounds/seatbelt.ogg',
 	'client/html/sounds/buckle.ogg',
-	'client/html/sounds/unbuckle.ogg'
+	'client/html/sounds/cell.ogg',
+	'client/html/sounds/demo.ogg',
+	'client/html/sounds/drop.ogg',
+	'client/html/sounds/lock.ogg',
+	'client/html/sounds/off.ogg',
+	'client/html/sounds/on.ogg',
+	'client/html/sounds/panic.ogg',
+	'client/html/sounds/PinFail.ogg',
+	'client/html/sounds/PinSucc.ogg',
+	'client/html/sounds/shake.ogg',
+	'client/html/sounds/unlock.ogg'
 })
